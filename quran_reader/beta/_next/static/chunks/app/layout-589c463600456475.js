ce8bb6195bb4f101bf93a06903ed267f1d616b48
omid3098 <omid3098@users.noreply.github.com>
2025-08-25 08:34:20 +0000

    Deploy beta: f0abafb3355bb5b796d7f7c7550eb283ffee92f8 f0abafb3355bb5b796d7f7c7550eb283ffee92f8

---
+++ quran_reader/beta/_next/static/chunks/app/layout-589c463600456475.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{444:function(n,e,u){Promise.resolve().then(u.t.bind(u,1448,23))},1448:function(){}},function(n){n.O(0,[793,34,739,744],function(){return n(n.s=444)}),_N_E=n.O()}]);
